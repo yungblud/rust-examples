7bc486c870d9de12ecc9372cad16b191e31ae4b8
yungblud <killi8n@gmail.com>
2023-06-18 20:15:40 +0900

    feat: new exmaple

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use std::fmt;
+
+struct Structure(i32);
+
+#[derive(Debug)]
+struct MinMax(i64, i64);
+
+impl fmt::Display for MinMax {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.0, self.1)
+    }
+}
+
+#[derive(Debug)]
+struct Point2D {
+    x: f64,
+    y: f64,
+}
+
+impl fmt::Display for Point2D {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "x: {}, y: {}", self.x, self.y)
+    }
+}
+
+#[derive(Debug)]
+struct Complex {
+    real: f64,
+    img: f64,
+}
+
+fn main() {
+    let minmax = MinMax(0, 14);
+
+    println!("Compare structures");
+    println!("Display: {}", minmax);
+    println!("Debug: {:?}", minmax);
+
+    let big_range = MinMax(-300, 300);
+    let small_range = MinMax(-3, 3);
+
+    println!("The big range: {big}, the small range: {small}",
+            small = small_range,
+            big = big_range
+    );
+
+    let point = Point2D { x: 3.3, y: 7.2 };
+
+    println!("Compare points");
+    println!("Display: {}", point);
+    println!("Debug: {:?}", point);
+
+    let complex: Complex = Complex { real: 3.3, img: 7.2 };
+
+    println!("Display: {:?}", complex);
+}

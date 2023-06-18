ccb9961ed6ff9eb5d6a97326d71629f268aaa2cc
yungblud <killi8n@gmail.com>
2023-06-18 20:25:56 +0900

    feat: for

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::fmt;
+use std::fmt::{self, Formatter, Display};
 
 struct Structure(i32);
 
@@ -46,6 +46,37 @@ impl fmt::Display for List {
     }
 }
 
+struct City {
+    name: &'static str,
+    lat: f32,
+    lon: f32,
+}
+
+impl Display for City {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let lat_c = if self.lat >= 0.0 { 'N' } else { 'S' };
+        let lon_c = if self.lon >= 0.0 { 'E' } else { 'W' };
+
+        write!(f, "{}: {:.3}{} {:.3}{}",
+                self.name, self.lat.abs(), lat_c, self.lon.abs(), lon_c
+        )
+    }
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "RGB ({}, {}, {})",
+            self.red, self.green, self.blue
+        )
+    }
+}
+
+#[derive(Debug)]
+struct Color {
+    red: u8,
+    green: u8,
+    blue: u8,
+}
 
 fn main() {
     let minmax = MinMax(0, 14);
@@ -74,4 +105,22 @@ fn main() {
 
     let v = List(vec![1, 2, 3]);
     println!("{}", v);
+
+    for city in [
+        City { name: "Dublin", lat: 53.347778, lon: -6.259722 },
+        City { name: "Oslo", lat: 59.95, lon: 10.75 },
+        City { name: "Vancouver", lat: 49.25, lon: -123.1 },
+    ] {
+        println!("{}", city);
+    }
+
+    for color in [
+        Color { red: 128, green: 255, blue: 90 },
+        Color { red: 0, green: 3, blue: 254 },
+        Color { red: 0, green: 0, blue: 0 },
+    ] {
+        // Switch this to use {} once you've added an implementation
+        // for fmt::Display.
+        println!("{}", color);
+    }
 }
